49e0c57d5d6c82f8bd419f75f6ef246bfcdeec35
Christoph Außem <chris.aussem@gmail.com>
2015-12-16 14:20:36 +0100

    AkuaJs for UI5

--- src/Core/CoreBundle.js
+++ src/Core/CoreBundle.js
@@ -1,21 +1,3 @@
 function define(requited, funktion){
-	var factory = funktion();
+	MyBarChart = funktion();
 }
-
-
-
-
-
-
-
-
-define(["jquery", 'underscore-min', 'linq.min', 'jshashtable-2.1', 'Core/Toolkit', 'Core/DSL', 'Core/Element', 'Core/Dimension', 'Core/VisEl', 'Core/Tuple', 'Core/Domain',
-		'Core/Axis', 'Core/VisualGeruestQuery',
-		'Core/SameDomainTupleList', 'Core/HyperCube', 'Core/MultiTupleFilter', 'Core/DataProvider', 'Core/Crosslist','Core/DumbTupleStore'], function () {
-		    //alert("core laoded");
-		}
-);
-
-
-
-
