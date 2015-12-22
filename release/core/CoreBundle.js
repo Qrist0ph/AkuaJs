9813d0f7a078c804b234b932ef0c393280c94377
Christoph Aussem <chris.aussem3@gmail.com>
2015-12-22 09:32:36 +0100

    for release

--- release/core/CoreBundle.js
+++ release/core/CoreBundle.js
@@ -1,1 +1,1 @@
-define(['underscore-min', 'linq.min', 'jshashtable-2.1', 'Core/AkuaCore'], function () {}); 
+define(['underscore-min', 'linq.min', 'jshashtable-2.1', 'core/AkuaCore'], function () {}); 
