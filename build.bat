35daabb4c6061c9675a857170d057091e090b5b7
Christoph Außem <chris.aussem@gmail.com>
2015-12-23 09:00:23 +0100

    dist/plain

--- build.bat
+++ build.bat
@@ -1,14 +1,16 @@
 echo Building
-rd release /s /q
-robocopy src\ release\ *.js
-robocopy src\core\ release\core\
-robocopy src\Libs\ release\libs\  /s
-copy src\TestDriver1Axis0Pkm.html release\
+rd dist /s /q
+robocopy src\ dist\plain\ *.js
+robocopy src\core\ dist\plain\core\
+robocopy src\Libs\ dist\plain\libs\  /s
+copy src\TestDriver1Axis0Pkm.html dist\plain\
 
-cd Release
+cd dist
+cd plain
+ 
 md Tmp
 
-FOR %%X in ("*.js") DO java -jar ..\compiler.jar --language_in=ECMASCRIPT5  --js %%X --js_output_file tmp\%%X
+FOR %%X in ("*.js") DO java -jar ..\..\compiler.jar --language_in=ECMASCRIPT5  --js %%X --js_output_file tmp\%%X
 rm *.js
 copy tmp\*.* .
 rd tmp /s /q
@@ -17,10 +19,10 @@ del CoreBundle.js
 
 rem # noch Core Bundle in eine Datei
 md Tmp
-java -jar ..\..\compiler.jar --js *.js --js_output_file tmp\AkuaCore.js
+java -jar ..\..\..\compiler.jar --js *.js --js_output_file tmp\AkuaCore.js
 del *.* /q
 move tmp\AkuaCore.js .
 rd tmp /s /q
 echo define(['underscore-min', 'linq.min', 'jshashtable-2.1', 'core/AkuaCore'], function () {}); > CoreBundle.js
 
-cd ..\..
+cd ..\..\..
