e99f57a5dbceba7613684c755c435d9073db62f3
Christoph Aussem <chris.aussem3@gmail.com>
2016-01-04 15:07:43 +0100

    Leaflet

--- src/AkuaLeaflet.js
+++ src/AkuaLeaflet.js
@@ -8,8 +8,10 @@ define(['core/CoreBundle', "leaflet"], function () {
         this.height = configObject.height ? configObject.height : 400;
         this.InfoMessage = configObject.InfoMessage ? configObject.InfoMessage : "Info:";
         this.Unit = configObject.Unit ? configObject.Unit : "";
-        this.MapCenter = configObject.MapCenter ? configObject.MapCenter : [52.8, 13];
-        this.Zoom = configObject.Zoom ? configObject.Zoom : 8;
+        this.mapCenter = configObject.mapCenter ? configObject.mapCenter : [52.8, 13];
+        this.zoom = configObject.zoom ? configObject.zoom : 8;
+		this.geoJson = configObject.geoJson;
+		this.click = configObject.click;
     }
 
     _LeafletMap.prototype.getView = function () {
@@ -22,11 +24,10 @@ define(['core/CoreBundle', "leaflet"], function () {
     _LeafletMap.prototype.updateValues = function (ok) {
         var me = this;
 
-        if (!me.map) {
-            //todo rest ?
+        if (!me.map) {           
 
             //me.map =  L.map(me.divid).setView([52.8, 13], 8);
-            me.map = L.map(me.view[0]).setView(me.MapCenter, me.Zoom);
+            me.map = L.map(me.view[0]).setView(me.mapCenter, me.zoom);
             //var map = L.map('map').setView([37.8, -96], 4);
 
             /*
@@ -75,26 +76,13 @@ define(['core/CoreBundle', "leaflet"], function () {
 
         } // Ende Lazy Init
 
-
-
-
-
-
-        var geoJsonString = {
-            "type": "FeatureCollection", "features": [
-                            { "type": "Feature", "id": "AFG", "properties": { "name": "geoJsonStringnn" }, "geometry": { "type": "Polygon", "coordinates": [[[61.210817, 35.650072], [62.230651, 35.270664], [62.984662, 35.404041], [63.193538, 35.857166], [63.982896, 36.007957], [64.546479, 36.312073], [64.746105, 37.111818], [65.588948, 37.305217], [65.745631, 37.661164], [66.217385, 37.39379], [66.518607, 37.362784], [67.075782, 37.356144], [67.83, 37.144994], [68.135562, 37.023115], [68.859446, 37.344336], [69.196273, 37.151144], [69.518785, 37.608997], [70.116578, 37.588223], [70.270574, 37.735165], [70.376304, 38.138396], [70.806821, 38.486282], [71.348131, 38.258905], [71.239404, 37.953265], [71.541918, 37.905774], [71.448693, 37.065645], [71.844638, 36.738171], [72.193041, 36.948288], [72.63689, 37.047558], [73.260056, 37.495257], [73.948696, 37.421566], [74.980002, 37.41999], [75.158028, 37.133031], [74.575893, 37.020841], [74.067552, 36.836176], [72.920025, 36.720007], [71.846292, 36.509942], [71.262348, 36.074388], [71.498768, 35.650563], [71.613076, 35.153203], [71.115019, 34.733126], [71.156773, 34.348911], [70.881803, 33.988856], [69.930543, 34.02012], [70.323594, 33.358533], [69.687147, 33.105499], [69.262522, 32.501944], [69.317764, 31.901412], [68.926677, 31.620189], [68.556932, 31.71331], [67.792689, 31.58293], [67.683394, 31.303154], [66.938891, 31.304911], [66.381458, 30.738899], [66.346473, 29.887943], [65.046862, 29.472181], [64.350419, 29.560031], [64.148002, 29.340819], [63.550261, 29.468331], [62.549857, 29.318572], [60.874248, 29.829239], [61.781222, 30.73585], [61.699314, 31.379506], [60.941945, 31.548075], [60.863655, 32.18292], [60.536078, 32.981269], [60.9637, 33.528832], [60.52843, 33.676446], [60.803193, 34.404102], [61.210817, 35.650072]]] } },
-            ]
-        };
-        geoJsonString = berlin;
-
-
+       
+      
 
         var minMax = this.calculateMinMax();
         var maxV = minMax.max;
         var minV = minMax.min;
         var max = Math.abs(maxV) > Math.abs(minV) ? Math.abs(maxV) : Math.abs(minV);
-
-
        
         var labels = Enumerable.From(this.calculateBuckets(minV, maxV)).Select(function (x) {
             return '<i style="background:' + x.color + '"></i> ' +
@@ -126,11 +114,10 @@ define(['core/CoreBundle', "leaflet"], function () {
             me.info.update();
         }
 
-
         if (me.geojson) me.map.removeLayer(me.geojson);
         //Geojson decoden
         //todo Logik für Färbung aus Treemap holen
-        me.geojson = L.geoJson(geoJsonString, {
+        me.geojson = L.geoJson( this.geoJson, {
             style: function (feature) {
 
                 var tuple = me.axis0.Tuples().FirstOrDefault(null, function (t) { return t.ToCaption() == feature.properties.Name });
@@ -157,16 +144,18 @@ define(['core/CoreBundle', "leaflet"], function () {
                 layer.on({
                     mouseover: highlightFeature,
                     mouseout: resetHighlight,
-                    //click: zoomToFeature
+                    click: function(e){
+						if (me.click){
+					   var tuple = me.axis0.Tuples().FirstOrDefault(null, function (t) { return t.ToCaption() == e.target.feature.properties.Name });
+						var value = tuple ? me.getValue(tuple) : 0;
+					    me.click(tuple,value);
+					  }
+					}
                 });
             }
         }).addTo(me.map);
 
     };
-
-
-
-
 
 
 
