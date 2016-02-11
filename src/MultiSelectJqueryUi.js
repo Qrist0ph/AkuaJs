587fbf7f136a6c752d7ffa3eaeb5e6bdafe319ea
Christoph Aussem <chris.aussem3@gmail.com>
2016-02-11 14:43:46 +0100

    MultiSelect Filter

--- src/MultiSelectJqueryUi.js
+++ src/MultiSelectJqueryUi.js
@@ -3,8 +3,7 @@ define(['core/CoreBundle'], function () {
     MultiSelectJqueyUi.prototype.constructor = MultiSelectJqueyUi;       // Otherwise instances of Cat would have a constructor of Mammal 
 
     function MultiSelectJqueyUi(configObject) {
-        Apple.call(this, configObject);
-        this.options = configObject.options;
+        Apple.call(this, configObject);       
         var me = this;
     }
 
@@ -20,10 +19,20 @@ define(['core/CoreBundle'], function () {
 
     MultiSelectJqueyUi.prototype.getView = function () {
 
+		   var text = '<div> \
+	<style> \
+#feedback { font-size: 1.4em; } \
+#selectable .ui-selecting { background: #FECA40; } \
+#selectable .ui-selected { background: #F39814; color: white; } \
+#selectable { list-style-type: none; margin: 0; padding: 0; width: 350px; } \
+#selectable li { margin: 3px; padding: 1px; float: left; width: 90px; height: 60px;  text-align: center; } \
+</style> \
+	<ol id="selectable" /></div><div style = "clear: both;" />';
+	
         var me = this;
         this.view = $(text);
 
-        $.each(this.options, function (i, v) {
+        $.each(me.axis0.Tuples().ToArray(), function (i, v) {
             jQuery(me.view.children()[1]).append(
 				$('<li class="ui-state-default"></li>').html(v.ToCaption()).data('payload', v)
 			);
@@ -44,15 +53,7 @@ define(['core/CoreBundle'], function () {
     };
 
 
-    var text = '<div> \
-	<style> \
-#feedback { font-size: 1.4em; } \
-#selectable .ui-selecting { background: #FECA40; } \
-#selectable .ui-selected { background: #F39814; color: white; } \
-#selectable { list-style-type: none; margin: 0; padding: 0; width: 350px; } \
-#selectable li { margin: 3px; padding: 1px; float: left; width: 90px; height: 60px;  text-align: center; } \
-</style> \
-	<ol id="selectable"></div>';
+ 
 
     return function (configObject) { return new MultiSelectJqueyUi.prototype.constructor(configObject); };
 });
